07f366863e7fd966f3822836c81f2029b23f111c
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-25 19:26:55 +0530

    TC3_SizeFilteration_3

--- src/test/java/testCases/TC3_SizeFilteration_3.java
+++ src/test/java/testCases/TC3_SizeFilteration_3.java
@@ -7,22 +7,21 @@ import pageObjects.ProductsPage_POM;
 import pageObjects.SizeFilteration_POM;
 import testBase.BaseClass;
 
-public class TC2_SizeFilteration_2 extends BaseClass {
-
-	/* Test Case 2 :
-	 * Validate size filtering by selecting multiple sizes.
-	 * Exp_Results :- Selected sized clothes should be display.
+public class TC3_SizeFilteration_3 extends BaseClass {
+	
+	/* Test Case 3 :
+	 * Validate size filtering by selecting all sizes.
+	 * Exp_Results :- All sized clothes should be display.
 	 */
 	
 	@Test
-	public void sizeFinteration2() throws InterruptedException {
-		
+	public void sizeFilteratin3() throws InterruptedException {
 		SizeFilteration_POM afp = new SizeFilteration_POM(driver);
-		afp.selectMultipleSizes();
+		afp.selectAllSizes();
 		Thread.sleep(3000);
 		ProductsPage_POM pp = new ProductsPage_POM(driver);
 		pp.getProductName();
-		Assert.assertEquals(pp.getResultsCount(), 12);
+		Assert.assertEquals(pp.getResultsCount(), 16);
 	}
 	
 }
