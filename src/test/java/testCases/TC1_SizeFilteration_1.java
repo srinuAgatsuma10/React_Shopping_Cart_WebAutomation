20bc6908e1615776703f149a3f657d0a4e83a866
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-25 19:12:42 +0530

    TC1_SizeFilteration_1

--- src/test/java/testCases/TC1_SizeFilteration_1.java
+++ src/test/java/testCases/TC1_SizeFilteration_1.java
@@ -8,15 +8,16 @@ import pageObjects.SizeFilteration_POM;
 import testBase.BaseClass;
 
 public class TC1_SizeFilteration_1 extends BaseClass {
-	
+	/* Test Case 1 :
+	 * Validate size filtering by selecting one size.
+	 * Exp_Results :- XL sized clothes should be display.
+	 */
 	@Test
-	public void sizeFilteration1()throws Exception {
+	public void sizeFilteration1() throws InterruptedException {
 		
 		SizeFilteration_POM afp = new SizeFilteration_POM(driver);
 		afp.selectOneSize();
-		
-		Thread.sleep(2000);
-		
+		Thread.sleep(1000);
 		ProductsPage_POM pp = new ProductsPage_POM(driver);
 		pp.getProductName();
 		Assert.assertEquals(pp.getResultsCount(), 10);
